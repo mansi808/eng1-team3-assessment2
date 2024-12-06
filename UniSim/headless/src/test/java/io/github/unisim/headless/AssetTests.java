0a4eba295942f317f8313084d4f4d615365bb18b
Vadim Mitko <vmmmvmm@icloud.com>
2024-12-06 22:34:29 +0000

    Testing: renamed AssetsTests to AssetTests

--- UniSim/headless/src/test/java/io/github/unisim/headless/AssetTests.java
+++ UniSim/headless/src/test/java/io/github/unisim/headless/AssetTests.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
-public class AssetsTests extends AbstractHeadlessGdxTest {
+public class AssetTests extends AbstractHeadlessGdxTest {
   @Test
   public void testBuildingAssetExists() {
     // Restaurant
