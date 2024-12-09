17623afcec170093e3ae74b9b3bb2f188bf51c60
Vadim Mitko <vmmmvmm@icloud.com>
2024-12-09 16:35:10 +0000

    ScoreManagerTests: Implemented testUpdateScoreWithBuildingCount

--- UniSim/headless/src/test/java/io/github/unisim/headless/ScoreManagerTests.java
+++ UniSim/headless/src/test/java/io/github/unisim/headless/ScoreManagerTests.java
@@ -10,6 +10,7 @@ import io.github.unisim.Point;
 import io.github.unisim.ScoreManager;
 import io.github.unisim.building.Building;
 import io.github.unisim.building.BuildingType;
+import io.github.unisim.building.data.types.BuildingTuple;
 import io.github.unisim.world.World;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -22,16 +23,33 @@ public class ScoreManagerTests extends AbstractHeadlessGdxTest {
 
   public void testUpdateScoreWithBuildingCount() {
     
-    // ScoreManager TestScoreManager = new ScoreManager();
+    ScoreManager TestScoreManager = new ScoreManager();
 
-    // ArrayList<Building> buildings = new ArrayList<Building>();
+    ArrayList<Building> buildings = new ArrayList<Building>();
     
+    ArrayList<BuildingTuple> buildingCounts = new ArrayList<BuildingTuple>();
+
+    Building TestBuilding = new Building(
+      new Texture(Gdx.files.internal("buildings/library.png")),
+      0.0075f,
+      new Vector2(1.8f, -4.6f),
+      new Point(),
+      new Point(20, 12),
+      false,
+      BuildingType.LEARNING,
+      "Library"
+  );
 
 
-    // TestScoreManager.updateScore(TestBuilding, buildings, []);
+    TestScoreManager.updateScore(TestBuilding, buildings, buildingCounts);
 
-    // assertEquals(TestScoreManager.getScore(), 10);    
+    assertEquals(TestScoreManager.getScore(), 10);    
 
+    buildingCounts.add(new BuildingTuple(1, BuildingType.LEARNING));
+
+    TestScoreManager.updateScore(TestBuilding, buildings, buildingCounts);
+
+    assertEquals(TestScoreManager.getScore(), 18);   
 
   }
 
