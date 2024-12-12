c466f5feb510d99e40da9e0118a2136fb71d0a8f
Harry <HarryMcLean11@gmail.com>
2024-12-12 11:55:30 +0000

    Restored Timing Values

--- UniSim/core/src/main/java/io/github/unisim/ui/GameScreen.java
+++ UniSim/core/src/main/java/io/github/unisim/ui/GameScreen.java
@@ -45,7 +45,7 @@ public class GameScreen implements Screen {
     eventMenu = new EventMenu(stage);
     world = new World(scoreManager);
 //    timer = new Timer(300_000);
-    timer = new Timer(6000);
+    timer = new Timer(300000);
       infoBar = new InfoBar(stage, timer, world, scoreManager);
     buildingMenu = new BuildingMenu(stage, world);
     eventManager = new EventManager(timer, eventMenu, world.scoreManager);
